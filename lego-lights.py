ce35e10a4e658d57b814381106aa7bc5b0a30dd1
Simon Coakley <scoakley@slb.com>
2022-02-19 14:23:22 +0000

    Add GET, gen buttons, control individual lights

--- lego-lights.py
+++ lego-lights.py
@@ -14,6 +14,7 @@ tlc5947 = adafruit_tlc5947.TLC5947(spi, latch, num_drivers=DRIVER_COUNT)
 
 delay = 0.5
 
+range_count = DRIVER_COUNT * 24
 index = 24
 # for i in range(48):
 #	  tlc5947[i] = 4095
@@ -48,16 +49,28 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/control", methods=['POST'])
+@app.route("/control", methods=['GET', 'POST'])
 def control():
-    content = request.json
-    print(content)
-    value = 0
-    if content['task'] == 'on':
-      value = 4095
-    for i in range(48):
+  if request.method == 'GET':
+    arr = [0 for x in range(range_count)]
+    for i in range(range_count):
+      print(i)
+      arr[i] = tlc5947[i]
+    print(arr)
+    return jsonify(arr)
+
+  content = request.json
+  print(content)
+  value = 0
+  if content['task'] == 'on':
+    value = 4095
+  index = content['index']
+  if index == -1:
+    for i in range(range_count):
       tlc5947[i] = value
-    return jsonify({'task': content['task'] + ' done'}), 201
+  else:
+    tlc5947[index] = value
+  return jsonify({'task': content['task'] + ' done'}), 201
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
